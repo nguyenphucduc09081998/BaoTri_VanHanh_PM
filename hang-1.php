4615e2a8271a12b52e90bae88d94c89d99b51efe
tranngochai23 <tr.ngochai23@gmail.com>
2019-04-02 06:10:57 +0700

    chinh hover tieu de

--- hang-1.php
+++ hang-1.php
@@ -2,72 +2,86 @@
     <div class="row my-3">
         <div class="col-12 col-sm-8 border-right">
             <div class="news">
-                <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
-                <div class="news-title flex-column d-flex align-items-start justify-content-end">
-                    <h4 class="col-12 p-0">Tieu de tin tuc acs mbcx asw</h4>
-                    <span>Tieu de tin tuc acs mbcx asw</span>
-                </div>
+                <a href="http://google.com">
+                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    <div class="news-title flex-column d-flex align-items-start justify-content-end">
+                            <h4 class="col-12 p-0 hoverable">Tieu de tin tuc acs mbcx asw</h4>
+                        <span>Tieu de tin tuc acs mbcx asw</span>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="col-12 col-sm-4">
             <div class="news news-r">
-                <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
-                <div class="news-title flex-column d-flex align-items-start justify-content-end">
-                    <b class="col-12 p-0">Tieu de tin tuc acs mbcx asw</b>
-                </div>
+                <a href="http://facebook.com/nhutphuongit">
+                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    <div class="news-title flex-column d-flex align-items-start justify-content-end">
+                        <b class="col-12 p-0 hoverable sub-title">Tieu de tin tuc acs mbcx asw</b>
+                    </div>
+                </a>
             </div>
             <div class="news news-r">
-                <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
-                <div class="news-title flex-column d-flex align-items-start justify-content-end">
-                    <b class="col-12 p-0">Tieu de tin tuc acs mbcx asw</b>
-                </div>
+                <a href="#">
+                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    <div class="news-title flex-column d-flex align-items-start justify-content-end">
+                        <b class="col-12 p-0 hoverable sub-title">Tieu de tin tuc acs mbcx asw</b>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-12 col-sm-6">
-            <div class="row">
-                <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
-                <div class="col-12 col-sm-4">
-                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+            <a class="no-color" href="trang-1.php">
+                <div class="row">
+                    <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
+                    <div class="col-12 col-sm-4">
+                        <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    </div>
+                    <div class="col-12 col-sm-8">
+                        Mo ta ngan gon, noi dung tin tic...
+                    </div>
                 </div>
-                <div class="col-12 col-sm-8">
-                    Mo ta ngan gon, noi dung tin tic...
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-12 col-sm-6">
-            <div class="row">
-                <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
-                <div class="col-12 col-sm-4">
-                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+            <a class="no-color" href="#">
+                <div class="row">
+                    <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
+                    <div class="col-12 col-sm-4">
+                        <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    </div>
+                    <div class="col-12 col-sm-8">
+                        Mo ta ngan gon, noi dung tin tic...
+                    </div>
                 </div>
-                <div class="col-12 col-sm-8 ml-0">
-                    Mo ta ngan gon, noi dung tin tic...
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-12 col-sm-6">
-            <div class="row">
-                <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
-                <div class="col-12 col-sm-4">
-                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+            <a class="no-color" href="#">
+                <div class="row">
+                    <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
+                    <div class="col-12 col-sm-4">
+                        <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    </div>
+                    <div class="col-12 col-sm-8">
+                        Mo ta ngan gon, noi dung tin tic...
+                    </div>
                 </div>
-                <div class="col-12 col-sm-8 ml-0">
-                    Mo ta ngan gon, noi dung tin tic...
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-12 col-sm-6">
-            <div class="row">
-                <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
-                <div class="col-12 col-sm-4">
-                    <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+            <a class="no-color" href="#">
+                <div class="row">
+                    <h4 class="col-12">Tieu de tin tuc acs mbcx asw</h4>
+                    <div class="col-12 col-sm-4">
+                        <div class="news-photo" style="background-image: url(images/hang-01/messi.jpg)"></div>
+                    </div>
+                    <div class="col-12 col-sm-8">
+                        Mo ta ngan gon, noi dung tin tic...
+                    </div>
                 </div>
-                <div class="col-12 col-sm-8 ml-0">
-                    Mo ta ngan gon, noi dung tin tic...
-                </div>
-            </div>
+            </a>
         </div>
     </div>
 </div>
