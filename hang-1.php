3665638b8f16eb91a572d5c2c66643d4cbbc4440
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2019-04-08 17:22:22 +0700

    database and description page

--- hang-1.php
+++ hang-1.php
@@ -10,7 +10,7 @@
 		?>
         <div class="col-12 col-sm-8 border-right">
             <div class="news">
-                <a href="trang-1.php">
+                <a href="/tintucbongda.php?MaTinTucBongDa=<?php echo $rbongda["MaTinTucBongDa"];?>">
                     <div class="news-photo" style="background-image: url(<?php echo $rbongda["ImageBongDa"];?>)"></div>
                     <div class="news-title flex-column d-flex align-items-start justify-content-end">
                         <h4 class="col-12 p-0 hoverable"><?php echo $rbongda['TieuDeBongDa'];?></h4>
@@ -32,7 +32,7 @@
 		{	
 		?>
 			<div class="news news-r">
-                <a href="">
+                <a href="/tintucbongro.php?MaTinTucBongRo=<?php echo $rbongro["MaTinTucBongRo"];?>">
                     <div class="news-photo" style="background-image: url(<?php echo $rbongro["ImageBongRo"];?>)"></div>
                     <div class="news-title flex-column d-flex align-items-start justify-content-end">
                         <b class="col-12 p-0 hoverable sub-title"><?php echo $rbongro['TieuDeBongRo'];?></b>
@@ -59,7 +59,7 @@
 		{	
 		?>
 		<div class="col-12 col-sm-6">
-            <a class="no-color" href="ChiTietThongTin.php">
+            <a class="no-color" href="/tintucvothuat.php?MaTinTucVoThuat=<?php echo $rvothuat["MaTinTucVoThuat"]; ?>">
                 <h4 class="col-12"><?php echo $rvothuat['TieuDeVoThuat'];?></h4>
                 <div class="col-12 col-sm-4">
                 <div class="news-photo" style="background-image: url(<?php echo $rvothuat["ImageVoThuat"];?>)"></div>
